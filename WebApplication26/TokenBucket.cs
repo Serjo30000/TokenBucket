0e0eba05caa495454e6ce06a6a7c9ee51e1eabd7
LAPTOP-1Q0NKG6M\Serge <Sergey2018sar@yandex.ru>
2024-03-23 00:16:49 +0400

    init project

---
+++ WebApplication26/TokenBucket.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication26
+{
+    public class TokenBucket
+    {
+        private BlockingCollection<Token> _tokens;
+        private System.Timers.Timer _timer;
+        private int _maxTokens;
+
+        public TokenBucket(int maxNumberOfTokens, int refillRateInMilliseconds)
+        {
+            _maxTokens = maxNumberOfTokens;
+            _timer = new System.Timers.Timer(refillRateInMilliseconds);
+            _tokens = new BlockingCollection<Token>(maxNumberOfTokens);
+            Init(maxNumberOfTokens);
+        }
+
+        public void UseToken()
+        {
+            if (!_tokens.TryTake(out Token _))
+            {
+                throw new RateLimiterException();
+            }
+        }
+
+        private void Init(int maxNumberOfTokens)
+        {
+            foreach (var _ in Enumerable.Range(0, maxNumberOfTokens))
+                _tokens.Add(new Token());
+
+            _timer.AutoReset = true;
+            _timer.Enabled = true;
+            _timer.Elapsed += OnTimerElapsed;
+        }
+
+        private void OnTimerElapsed(object? sender, System.Timers.ElapsedEventArgs e)
+        {
+            foreach (var _ in Enumerable.Range(0, _maxTokens - _tokens.Count))
+                _tokens.Add(new Token());
+        }
+    }
+    public record Token;
+}
---
+++ WebApplication26/TokenBucketRateLimiterMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication26
+{
+    public class TokenBucketRateLimiterMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public TokenBucketRateLimiterMiddleware(RequestDelegate requestDelegate)
+        {
+            _next = requestDelegate;
+        }
+
+        public async Task InvokeAsync(HttpContext context, TokenBucket bucket)
+        {
+            try
+            {
+                bucket.UseToken();
+                await _next(context);
+            }
+            catch (NoTokensAvailableException)
+            {
+                context.Response.StatusCode = 503;
+            }
+        }
+    }
+}
